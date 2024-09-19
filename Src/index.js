3ecde64363682ae9790a9d87e92ef10d21fdff05
春秋老怪 <aspnmy@users.noreply.github.com>
2024-09-20 05:55:44 +0800

    Update index.js

---
+++ Src/index.js
@@ -0,0 +1,19 @@
+(() => {
+  // src/index.js
+  var OPENAI_API_HOST = VSDT;
+  addEventListener("fetch", (event) => {
+    event.respondWith(handleRequest(event.request));
+  });
+  async function handleRequest(request) {
+    const url = new URL(request.url);
+    url.host = OPENAI_API_HOST;
+    const newRequest = new Request(url.toString(), {
+      method: request.method,
+      headers: request.headers,
+      body: request.body,
+      redirect: "follow"
+    });
+    return await fetch(newRequest);
+  }
+})();
+//# sourceMappingURL=index.js.map
